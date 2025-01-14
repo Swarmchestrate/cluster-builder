64b625c5d72a233d7ceed0e1aa5ff8460792f5d2
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2025-01-14 14:52:25 +0000

    feat: scaffold cluster-builder.py

--- cluster-builder/cluster-builder.py
+++ cluster-builder/cluster-builder.py
@@ -1,1 +1,36 @@
-# Placeholder
+import jinja2
+
+def create():
+
+    """
+    Create a new k3s cluster with OpenTofu
+    """
+    
+    substitute_values()
+
+    deploy()
+
+    configure()
+
+
+def substitute_values():
+    """
+    Function to substitute values in the opentofu configuration file
+    """
+    
+    raise NotImplementedError
+
+
+def deploy():
+    """
+    Function to deploy the infrastructure
+    """
+    
+    raise NotImplementedError
+
+def configure():
+    """
+    Function to configure the k3s cluster
+    """
+    
+    raise NotImplementedError
