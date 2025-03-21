0838426dfb45821fd31fc1f54646f9b0d5b61f18
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2025-03-21 15:37:32 +0000

    tests: show example of add

--- scripts/test_deploy.py
+++ scripts/test_deploy.py
@@ -1,8 +1,9 @@
-
 from config import aws_config, pg_config
 from cluster_builder.cluster_builder import Swarmchestrate
 
-#aws_config["cluster_name"] = "swarmchestrate_fervent_rubin"
+# aws_config["cluster_name"] = "crazy_wescoff"
+# aws_config["k3s_role"] = "worker"
+# aws_config["master_ip"] = "x.x.x.x"
 
 swarmchestrate = Swarmchestrate(template_dir="templates", output_dir="output", pg_config=pg_config)
 swarmchestrate.prepare_modules(aws_config)
