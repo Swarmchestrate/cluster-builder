f160d162ce8f093d88dfbec1111a9b440e6fe757
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2025-03-28 20:36:17 +0000

    fix: fixup script

--- scripts/test_remove.py
+++ scripts/test_remove.py
@@ -1,10 +1,8 @@
-from config import pg_config
 from cluster_builder import Swarmchestrate
 
 CLUSTER_NAME = "boring_liskov"
 NODE_NAME = "aws_sweet_swanson"
 
-swarmchestrate = Swarmchestrate(
-    template_dir="templates", output_dir="output", pg_config=pg_config
-)
+
+swarmchestrate = Swarmchestrate(template_dir="templates", output_dir="output")
 swarmchestrate.remove_node(CLUSTER_NAME, NODE_NAME)
