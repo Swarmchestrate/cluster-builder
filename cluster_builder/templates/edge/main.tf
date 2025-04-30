8a6258f387918f047cd3958add33e85888a62b3a
Gunjan Kotak <57281121+kotak-Gunjan@users.noreply.github.com>
2025-04-30 13:50:59 +0000

    feat: swch edge module from jinja to hcl

---
+++ cluster_builder/templates/edge/main.tf
@@ -0,0 +1,66 @@
+# variables.tf
+variable "cluster_name" {}
+variable "edge_device_ip" {}
+variable "k3s_token" {}
+variable "cloud" {
+  default = null
+}
+variable "k3s_role" {}
+variable "resource_name" {}
+variable "ssh_auth_method" {}
+variable "ssh_user" {}
+variable "ssh_password" {
+  sensitive = true
+  default = null
+}
+variable "ssh_private_key" {
+  default = null
+}
+variable "master_ip" {
+  default = null
+}
+variable "ha" {
+  default = null
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/${var.k3s_role}_user_data.sh.tpl")
+  vars = {
+    k3s_token = var.k3s_token
+    ha        = var.ha
+  }
+}
+
+resource "local_file" "rendered_user_data" {
+  content  = data.template_file.user_data.rendered
+  filename = "${path.module}/${var.k3s_role}_user_data.sh"
+}
+
+resource "null_resource" "deploy_k3s_edge" {
+  connection {
+    type        = "ssh"
+    user        = var.ssh_user
+    host        = var.edge_device_ip
+    password    = var.ssh_auth_method == "password" ? var.ssh_password : null
+    private_key = var.ssh_auth_method == "key" ? file(var.ssh_private_key) : null
+  }
+
+   provisioner "file" {
+    source      = "${path.module}/${var.k3s_role}_user_data.sh"
+    destination = "/tmp/edge_user_data.sh"
+   }
+
+   provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/edge_user_data.sh",
+      "sudo /tmp/edge_user_data.sh"
+    ]
+  }
+
+  triggers = {
+    Name          = "K3s-${var.k3s_role}-${var.cluster_name}-${var.resource_name}"
+    cluster_name  = var.cluster_name
+    role          = var.k3s_role
+    resource_name = var.resource_name
+  }
+}
