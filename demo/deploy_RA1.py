fa8ed18bbbd6945bdc8953ebe9f610767a9f9948
Gunjan Kotak <57281121+kotak-Gunjan@users.noreply.github.com>
2025-08-22 14:35:00 +0000

    feat: added demo scripts

---
+++ demo/deploy_RA1.py
@@ -0,0 +1,31 @@
+import json
+from cluster_builder import Swarmchestrate
+from .utils import read_outputs
+import logging
+
+logger = logging.getLogger("demo_RA1")
+
+def read_input_config():
+    with open("demo/RA1.json") as f:
+        return json.load(f)
+
+# Script execution
+cluster_config = read_input_config()
+base_data = read_outputs()  # reads lead_RA_outputs.json
+
+cluster_name = base_data["cluster_name"]
+master_ip = base_data["master_ip"]
+k3s_token = base_data["k3s_token"]
+
+swarmchestrate = Swarmchestrate(template_dir="templates", output_dir="output")
+
+for node in cluster_config["nodes"]:
+    node.update({
+        "k3s_token": k3s_token,
+        "master_ip": master_ip,
+        "cluster_name": cluster_name,
+    })
+    logger.info(f"[INFO] Deploying {node['k3s_role']} node on cloud: {node['cloud']}")
+    swarmchestrate.add_node(node)
+
+logger.info(f"[INFO] Cluster {cluster_name} updated with nodes from this RA.") 
