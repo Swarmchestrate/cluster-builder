59a18f9e3d811aca6d7854bf1408b02dcb52ebc0
Gunjan Kotak <57281121+kotak-Gunjan@users.noreply.github.com>
2025-09-16 08:37:24 +0000

    fix: remove the demo json

--- demo/destroy.py
+++ demo/destroy.py
@@ -1,6 +1,6 @@
 from cluster_builder import Swarmchestrate
 
-CLUSTER_NAME = "great-benz"
+CLUSTER_NAME = "vigorous-panini"
 
 swarmchestrate = Swarmchestrate(template_dir="templates", output_dir="output")
 swarmchestrate.destroy(CLUSTER_NAME)
